5f905b471ed1875cea81c0e9d21f78c774785c02
DouglasHdezT <00004516@uca.edu.sv>
2019-11-04 16:04:46 -0600

    Fix: Typo errors

--- bin/config.js
+++ bin/config.js
@@ -6,7 +6,7 @@ let host = "localhost";
 let uri = `mongodb://${host}:${port}/${database}`
 
 const connect = ()=>{
-    Mongoose.connect(uri, { useNewUrlParser: true })
+    Mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>{
         console.log(`Conection to ${database} successfully`);
     })
